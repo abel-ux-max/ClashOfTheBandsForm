d79169ff2088f58e8e29fcbf67a5b5c0ff056104
Abel <abel.elersic@gmail.com>
2025-11-02 18:17:49 +0100

    Add files via upload

--- bitkabendov/js/form.js
+++ bitkabendov/js/form.js
@@ -9,12 +9,31 @@ function showStep(i) {
 
 document.querySelectorAll(".next").forEach(btn => {
   btn.addEventListener("click", () => {
-    const inputs = steps[current].querySelectorAll("input[required]");
+    const inputs = steps[current].querySelectorAll("input[required], textarea[required]");
     let allValid = true;
     inputs.forEach(inp => {
       if (!inp.checkValidity()) {
         inp.reportValidity();
         allValid = false;
+        if (lang.startsWith("en")) {
+          Swal.fire({
+            icon: "warning",
+            title: "Missing fields",
+            text: "Please fill in all required fields before submitting.",
+            confirmButtonColor: "#9c0000"
+          });
+        }
+        else if (lang.startsWith("sl")) {
+          Swal.fire({
+            icon: "warning",
+            title: "Prazna polja",
+            text: "Prosimo izpolnite vsa polja pravilno pred oddajo.",
+            confirmButtonColor: "#9c0000"
+          });
+        }
+
+        return;
+
       }
     });
     if (allValid && current < steps.length - 1) current++;
@@ -27,4 +46,38 @@ document.querySelectorAll(".prev").forEach(btn => {
     if (current > 0) current--;
     showStep(current);
   });
+});
+
+
+document.querySelector("#signform").addEventListener("submit", e => {
+  e.preventDefault();
+
+
+
+  if (lang.startsWith("en")) {
+    Swal.fire({
+      icon: "success",
+      title: "Submission successful!",
+      text: "Thank you for registering for the Clash of the Bands.",
+      confirmButtonColor: "#9c0000"
+    }).then(() => {
+      document.querySelector("#signform").reset();
+      current = 0;
+      showStep(current);
+    });
+  }
+  else if (lang.startsWith("sl")) {
+    Swal.fire({
+      icon: "success",
+      title: "Oddaja uspešna!",
+      text: "Hvala za prijavo na Bitko bendov.",
+      confirmButtonColor: "#9c0000"
+    }).then(() => {
+      document.querySelector("#signform").reset();
+      current = 0;
+      showStep(current);
+    });
+  }
+
+
 });
