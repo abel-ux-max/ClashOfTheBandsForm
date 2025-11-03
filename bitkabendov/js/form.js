6b8c3064c1733445a2a0118574f7b9860f36fdd4
Abel <abel.elersic@gmail.com>
2025-11-03 06:33:20 +0100

    Add files via upload

--- bitkabendov/js/form.js
+++ bitkabendov/js/form.js
@@ -1,6 +1,7 @@
 const steps = document.querySelectorAll(".step");
 const progress = document.querySelectorAll(".progress div");
-
+const lang = document.documentElement.lang || "sl";
+ 
 let current = 0;
 
 function showStep(i) {
@@ -18,8 +19,8 @@ document.querySelectorAll(".next").forEach(btn => {
         allValid = false;
         Swal.fire({
         icon: "warning",
-        title: "Missing fields!" ,
-        text: "Please fill in all required fields before submitting." ,
+        title: lang.startsWith("en") ? "Missing fields!" : "Prazna polja!",
+        text: lang.startsWith("en") ? "Please fill in all required fields before submitting." : "Prosimo, izpolnite vsa zahtevana polja pravilno pred oddajo.",
         confirmButtonColor: "#9c0000"
       });
 
@@ -31,22 +32,30 @@ document.querySelectorAll(".next").forEach(btn => {
     showStep(current);
   });
 });
+
 document.querySelectorAll(".prev").forEach(btn => {
   btn.addEventListener("click", () => {
     if (current > 0) current--;
     showStep(current);
   });
 });
+
+
 document.querySelector("#signform").addEventListener("submit", e => {
   e.preventDefault();
+
+
     Swal.fire({
     icon: "success",
-    title: "Submission successful!",
-    text: "Thank you for registering for the Battle of the Bands.",
+    title: lang.startsWith("en") ? "Submission successful!" : "Oddaja uspešna!",
+    text: lang.startsWith("en") ? "Thank you for registering for the Battle of the Bands." : "Hvala za prijavo na Bitko bendov.",
     confirmButtonColor: "#9c0000"
     }).then(() => {
       document.querySelector("#signform").reset();
       current = 0;
       showStep(current);
     });
+  
+
+
 });
