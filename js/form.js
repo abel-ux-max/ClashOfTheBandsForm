e36e89d6012f737936d4432bbccad52a46a4af68
abel-ux-max <abel.elersic@gmail.com>
2025-11-14 10:06:45 +0100

    Enhance form validation and styling for user inputs

--- js/form.js
+++ js/form.js
@@ -1,10 +1,10 @@
 const steps = document.querySelectorAll(".step");
 const progress = document.querySelectorAll(".progress div");
 const lang = document.documentElement.lang || "sl";
- 
+/* burek velikonocno jajce*/
 let current = 0;
 
-function showStep(i) { 
+function showStep(i) {
   for (var idx = 0; idx < steps.length; idx++) {
     steps[idx].classList.toggle("active", idx === i);
   }
@@ -18,15 +18,51 @@ document.querySelectorAll(".next").forEach(btn => {
     const inputs = steps[current].querySelectorAll("input[required], textarea[required]");
     let allValid = true;
     inputs.forEach(inp => {
+      inp.classList.remove("error");
       if (!inp.checkValidity()) {
         inp.reportValidity();
         allValid = false;
-        Swal.fire({
-        icon: "warning",
-        title: lang.startsWith("en") ? "Missing fields!" : "Prazna polja!",
-        text: lang.startsWith("en") ? "Please fill in all required fields before submitting." : "Prosimo, izpolnite vsa zahtevana polja pravilno pred oddajo.",
-        confirmButtonColor: "#9c0000"
-      });
+        if (inp.validity.valueMissing) {
+          Swal.fire({
+            icon: "warning",
+            title: lang.startsWith("en") ? "Missing fields!" : "Prazna polja!",
+            text: lang.startsWith("en") ? "Please fill in all required fields before submitting." : "Prosimo, izpolnite vsa zahtevana polja pravilno pred oddajo.",
+            confirmButtonColor: "#9c0000"
+          });
+          inp.classList.toggle("error");
+        }
+        if(inp.validity.patternMismatch){
+          Swal.fire({
+            icon: "warning",
+            title: lang.startsWith("en") ? "Wrong number format!" : "Napačen format telefonske številke!",
+            text: lang.startsWith("en") ? "Please fill in the rquired pattern 031 230 230." : "Prosimo, izpolnite v zaželeni obliki 031 230 120.",
+            confirmButtonColor: "#9c0000"
+          });
+          inp.classList.toggle("error");
+        }
+        if(inp.validity.typeMismatch){
+          if(inp.type === "email"){
+            Swal.fire({
+            icon: "warning",
+            title: lang.startsWith("en") ? "Email is not valid!" : "Email ni prou bumbar!",
+            text: lang.startsWith("en") ? "Please make sure that the email is valid." : "Email mora biti veljaven!",
+            confirmButtonColor: "#9c0000"
+          });
+          inp.classList.toggle("error");
+          }
+          if(inp.type === "url"){
+            Swal.fire({
+            icon: "warning",
+            title: lang.startsWith("en") ? "Email is not valid!" : "Povezava ni pravilna!",
+            text: lang.startsWith("en") ? "Please make sure that the email is valid." : "Povezava mora biti veljavna...s",
+            confirmButtonColor: "#9c0000"
+          });
+          inp.classList.toggle("error");
+          }
+          
+        }
+          
+
 
         return;
 
@@ -34,6 +70,7 @@ document.querySelectorAll(".next").forEach(btn => {
     });
     if (allValid && current < steps.length - 1) current++;
     showStep(current);
+    inp.classList.remove("error");
   });
 });
 
@@ -49,17 +86,17 @@ document.querySelector("#signform").addEventListener("submit", e => {
   e.preventDefault();
 
 
-    Swal.fire({
+  Swal.fire({
     icon: "success",
     title: lang.startsWith("en") ? "Submission successful!" : "Oddaja uspešna!",
     text: lang.startsWith("en") ? "Thank you for registering for the Battle of the Bands." : "Hvala za prijavo na Bitko bendov.",
     confirmButtonColor: "#9c0000"
-    }).then(() => {
-      document.querySelector("#signform").reset();
-      current = 0;
-      showStep(current);
-    });
-  
+  }).then(() => {
+    document.querySelector("#signform").reset();
+    current = 0;
+    showStep(current);
+  });
+
 
 
 });
